1513499f35b5d25fb0c17c01a7822ce50b51e9d3
julpian <lutfijulpian@gmail.com>
2025-11-11 08:55:36 +0700

    mempebaiki halaman login

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -14,7 +14,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8080/api/admin/login", {
+      const res = await fetch("http://127.0.0.1:8080/api/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
