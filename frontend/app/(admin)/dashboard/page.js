a2bb9a6f7601b074162bbaef26121cc6893267b2
julpian <lutfijulpian@gmail.com>
2025-11-13 12:14:16 +0700

    delete influencer2

--- frontend/app/(admin)/dashboard/page.js
+++ frontend/app/(admin)/dashboard/page.js
@@ -71,7 +71,7 @@ export default function DashboardPage() {
     try {
       // PERBAIKAN BUG KRITIS: Panggil rute /api/admin/... dan sertakan token
       //const res = await fetch(`http://127.0.0.1:8080/api/admin/influencers/${id}`, {
-      const res = await fetch(`http://127.0.0.1:8080/api/admin/influencers/${id}`, {
+      const res = await fetch(`/api/admin/influencers/${id}`, {
         method: "DELETE",
         headers: {
             'Authorization': `Bearer ${token}`
